7104ec4ac58158fa5d903f46caf2ef8a14e5ca7e
wming18082721229-design <wming18082721229@gmail.com>
2025-11-14 10:38:44 +0000

    Add files via upload

---
+++ NumMatch/NumMatch/mydefs.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/* Put other #includes here,
+   your struct board, helper function
+   prototypes etc.
+*/
+
+#define HEIGHT 4
+#define WIDTH 5
+#define BIGSIZE 100000
+#define MATCH ((p->b[z.y1][z.x1] == p->b[z.y2][z.x2]) || (p->b[z.y1][z.x1]+p->b[z.y2][z.x2] == 10)) && (p->b[z.y1][z.x1]!=0 && p->b[z.y2][z.x2]!=0)
+
+struct board {
+   int b[HEIGHT][WIDTH];
+};
+typedef struct board board;
+
+bool check_horizontal_gap(board* p, pair z);
+bool check_vertical_gap(board* p, pair z);
+bool check_diagonal_gap(board* p, pair z);
+bool is_connected(board* p, pair z);
+bool is_unique(board tmp, board* p, int start, int end);
+bool is_final(board* p);
